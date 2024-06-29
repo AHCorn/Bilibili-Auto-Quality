9e31333800f67b9027ab9cbe9ed1b29ea01cad55
安和 <42889600+AHCorn@users.noreply.github.com>
2024-06-30 00:43:37 +0800

    Update Bilibili-Auto-Quality.user.js
    From https://greasyfork.org/zh-TW/scripts/441403-bilibili-%E8%A7%A3%E9%8E%96%E6%9D%9C%E6%AF%94%E5%85%A8%E6%99%AF%E8%81%B2-8k-%E9%96%8B%E5%95%9F-hdr-%E7%9B%B4%E6%92%AD%E7%95%AB%E8%B3%AApro

--- Bilibili-Auto-Quality.user.js
+++ Bilibili-Auto-Quality.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         哔哩哔哩自动画质
 // @namespace    https://github.com/AHCorn/Bilibili-Auto-Quality/
-// @version      2.6
+// @version      2.6.1
 // @license      GPL-3.0
 // @description  自动解锁并更改哔哩哔哩视频的画质和音质，实现自动选择最高画质、无损音频及杜比全景声。
 // @author       安和（AHCorn）
@@ -27,7 +27,7 @@
     'use strict';
 
     Object.defineProperty(navigator, 'userAgent', {
-        value: "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_3_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2 Safari/605.1.15"
+        value: "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15"
     });
 
     window.localStorage['bilibili_player_force_DolbyAtmos&8K&HDR'] = 1;
