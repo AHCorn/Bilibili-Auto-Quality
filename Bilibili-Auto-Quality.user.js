12b97e79f29a8db405da978dce33f8a94cb0ada5
安和 <42889600+AHCorn@users.noreply.github.com>
2024-07-18 05:00:03 +0800

    Update Bilibili-Auto-Quality.user.js

--- Bilibili-Auto-Quality.user.js
+++ Bilibili-Auto-Quality.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         哔哩哔哩自动画质
 // @namespace    https://github.com/AHCorn/Bilibili-Auto-Quality/
-// @version      3.0.2
+// @version      3.0.3
 // @license      GPL-3.0
 // @description  自动解锁并更改哔哩哔哩视频的画质和音质，实现自动选择最高画质、无损音频及杜比全景声。
 // @author       安和（AHCorn）
